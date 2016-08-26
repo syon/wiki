ddb2bcbc874551450ea53744f26c0c683383a366
syon <syon.xiii@gmail.com>
2016-08-27 03:58:28 +0900

    serve.js

--- serve.js
+++ serve.js
@@ -1,7 +1,14 @@
+/* eslint no-console: "off" */
 const nodeStatic = require('node-static');
+const dotenv     = require('dotenv');
 
-var serve = new nodeStatic.Server(__dirname);
+dotenv.config();
+
+console.log(`Listening Port: ${process.env.LISTEN_PORT}`);
+console.log(`Serving directory: ${process.env.SERVE_DIR}`);
+
+const serve = new nodeStatic.Server(`${__dirname}/${process.env.SERVE_DIR}`);
 require('http').createServer((req, res) => {
   req.addListener('end', () => serve.serve(req, res));
   req.resume();
-}).listen(8888);
+}).listen(process.env.LISTEN_PORT);
