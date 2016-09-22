cba39c94d1f3d5faa6ca5e76afadfd101486afe8
syon <syon.xiii@gmail.com>
2016-09-22 11:45:20 +0900

    metalsmith-watch

--- build.js
+++ build.js
@@ -7,6 +7,26 @@ var jade       = require('metalsmith-jade');
 var layouts    = require('metalsmith-layouts');
 var permalinks = require('metalsmith-permalinks');
 var mapsite    = require('metalsmith-mapsite');
+var watch      = require('metalsmith-watch');
+var dotenv     = require('dotenv').config();
+
+var NODE_ENV = process.env.NODE_ENV;
+if (dotenv.NODE_ENV) {
+  NODE_ENV = dotenv.NODE_ENV;
+}
+
+function watching() {
+  if (NODE_ENV == 'development') {
+    return watch({
+      paths: {
+        "${source}/**/*": "**/*",
+        "layouts/**/*": "**/*",
+        "assets/**/*": "**/*"
+      },
+      livereload: true,
+    });
+  }
+}
 
 Metalsmith(__dirname)
   .destination('wiki')
@@ -32,6 +52,7 @@ Metalsmith(__dirname)
   .use(mapsite({
     hostname: 'https://syon.github.io/wiki/'
   }))
+  .use(watching())
   .build(function(err){
     if (err) throw err;
   });
